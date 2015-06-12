ed2697bd8937a3752878ec049d16dc64a517de58
Da Wei <xichuanglian@gmail.com>
2015-06-12 15:57:42 +0800

    rpc server and client example

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,4 +1,7 @@
 var error_page = require("../../lib/error_page.js");
+var dnode = require("dnode");
+var path = require("path");
+var CONFIG = require(path.join(process.cwd(), "config.json"));
 
 exports.GET = function(req, res) {
     res.writeHead(200, {
@@ -28,5 +31,29 @@ exports.query = {
                 res.end();
             }
         });
+    }
+};
+
+exports.rpc = {
+    "GET" : function(req, res) {
+        var server_id = parseInt(req.query.server);
+        var addr = CONFIG.RPC_SERVERS[server_id];
+        if (typeof(addr) === "string" || addr instanceof String) {
+            var pos = addr.indexOf(':');
+            var host = addr.substr(0, pos);
+            var port = parseInt(addr.substr(pos + 1));
+            var d = dnode.connect({host: host, port: port});
+            d.on('remote', function (remote) {
+                remote.hello("server " + req.server_id, function (reply) {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/plain'
+                    });
+                    res.write(reply);
+                    res.end();
+                });
+            });
+        } else {
+            error_page.render(400, req, res, "Server ID Error");
+        }
     }
 };
